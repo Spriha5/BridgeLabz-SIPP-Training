e921e7d0afeb23ca4c5567303b76c6be393738d6
Spriha <sprihasingh2016@gmail.com>
2025-06-18 10:47:14 +0530

    [Spriha] commit is first smallest

---
+++ Day2_ControlFlow/isfirstlargest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class isfirstlargest {
+public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+  if(a<b && a<c )
+  {
+    System.out.print("first number is smallest");
+  }
+  else{
+   System.out.print("first number is not smallest");
+  }
+}
+}
