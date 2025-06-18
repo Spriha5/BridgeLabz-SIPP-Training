b9e3253acdd205bb90fa380f3986ab7d968f34fb
Spriha <sprihasingh2016@gmail.com>
2025-06-18 10:54:28 +0530

    [Spriha] commit is divisible by 5

---
+++ Day2_ControlFlow/Divisible_by_5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Divisible_by_5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+          if(n%5==0)
+          {
+           System.out.println("Is divisible by five");
+          }
+    
+    }
+    
+    
+}
