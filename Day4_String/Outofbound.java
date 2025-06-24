a2092ae1609f8001c47bfa97cb5383ff774b3743
Spriha <sprihasingh2016@gmail.com>
2025-06-24 10:44:42 +0530

    [Spriha] out of bound error

--- Day4_String/Outofbound.java
+++ Day4_String/Outofbound.java
@@ -2,14 +2,15 @@ import java.util.*;
 public class Outofbound{
   public static void main(String[] args) {
      Scanner sc=new Scanner(System.in);
-     int n=10;
+     int n=9;
      int[] arr=new int[n];
 
-     for(int i=0;i<10;i++)
+     for(int i=0;i<9;i++)
      {
         arr[i]=sc.nextInt();
      }
 
-     System.out.println(arr[10]);
+     System.out.println(arr[9]);
+     sc.close();
     }
 }
