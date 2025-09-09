e1fc6cdcfdff051f743d577d147985469848c4c1
Spriha <sprihasingh2016@gmail.com>
2025-09-09 21:47:17 +0530

    all names to uppercase

---
+++ Day1_LambdaExpression/UpperCase.java
@@ -0,0 +1,15 @@
+package Day1_LambdaExpression;
+import java.util.*;
+public class UpperCase {
+	
+	    public static void main(String[] args) {
+	        List<String> employees = Arrays.asList("Amit", "Spriha", "Neha", "Ravi");
+
+	        System.out.println(" Employee names in UPPERCASE:");
+	        employees.stream()
+	                 .map(String::toUpperCase)  
+	                 .forEach(System.out::println);
+	    }
+	}
+
+
